e01bdca4065c724ff84bd8ce4983bbc56db7c9f1
Md. Jahid Hasan Joy <jahid.joy.cu@gmail.com>
2024-11-11 01:19:05 +0600

    Add files via upload

---
+++ if-else/electricity.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{   
+    double units, bill, totall_bill, totall_with_add;
+
+    printf("Enter the total units = ");
+    scanf("%lf", &units);
+
+    if (units <= 200)
+    {
+        bill = 0.80 * units;
+        totall_bill = bill + 100;
+        printf("The estimated charge is = %.2lf\n", totall_bill);
+    }
+    else if ( units > 200 && units <= 300)
+    {
+        bill = 160 + (units-200)*0.90;
+        totall_bill = bill + 100;
+        printf("The estimated charge is = %.2lf\n", totall_bill);
+        
+    }
+    else
+    {
+        bill = 250 + (units - 300);
+        totall_bill = bill + 100;
+
+        if( totall_bill > 400)
+        {
+            totall_with_add = totall_bill + (totall_bill*(15.00/100));
+            printf(" The estimated charge is = %.2lf\n",totall_with_add);
+        }
+        else
+        {
+        printf("The estimated charge is = %.2lf\n", totall_bill);
+        }
+    }
+    return 0;
+
+}
